a618a178781ecda8ea4d82c21b1c0752b1fddefb
your new username <smallmonster900@gmail.com>
2024-10-09 21:23:07 +0700

    add

--- src/main/java/com/example/controller/CategoryController.java
+++ src/main/java/com/example/controller/CategoryController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +22,18 @@ public class CategoryController {
         List<Category> categories = categoryService.findAll();
         model.addAttribute("categories",categories);
         return "category/index";
+    }
+    @GetMapping("/add")
+    public String add(Model model){
+        Category category = new Category();
+        model.addAttribute("category",category);
+        return "category/add";
+    }
+    @PostMapping("/add")
+    public String create(@ModelAttribute Category category){
+        if(categoryService.create(category)){
+            return "redirect:/category";
+        }
+        return "category/add";
     }
 }
